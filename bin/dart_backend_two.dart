a40ad99d68cc6a0a6566ef404c718961aacd0e72
Jadiel Santana <jadiel.bsantana@gmail.com>
2022-09-30 22:11:02 -0300

    ⭐️: First commit

---
+++ bin/dart_backend_two.dart
@@ -0,0 +1,5 @@
+import 'package:dart_backend_two/dart_backend_two.dart' as dart_backend_two;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_backend_two.calculate()}!');
+}
---
+++ test/dart_backend_two_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_backend_two/dart_backend_two.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
